93c02590aada8d05a27501c61eb66cff1f7da3e8
Pabloo22 <pablete.arino@gmail.com>
2023-03-26 20:10:58 +0200

    Minor refactoring

--- src/decision_making/srf.py
+++ src/decision_making/srf.py
@@ -25,16 +25,22 @@ class Srf:
         self.r_v = max(self.rank_dict.keys())
         self.weights = dict()
 
-        self._calculate_weights()
+        self._preprocess_rank_dict()
 
-    def _calculate_weights(self):
+    def _preprocess_rank_dict(self):
+        """Deletes the white card alternatives from the rank dictionary and ensures that each rank has a list of
+        alternatives."""
+
+        for rank in self.rank_dict:
+            if self.rank_dict[rank] == 'white_card':
+                del self.rank_dict[rank]
+            elif not isinstance(self.rank_dict[rank], list):
+                self.rank_dict[rank] = [self.rank_dict[rank]]
+
+    def calculate_weights(self):
         for rank in self.rank_dict.keys():
-            if self.rank_dict[rank] != 'white_card':
-                if isinstance(self.rank_dict[rank], list):
-                    for alternative in self.rank_dict[rank]:
-                        self.weights[alternative] = self.__calculate_weight(rank)
-                else:
-                    self.weights[self.rank_dict[rank]] = self.__calculate_weight(rank)
+            for alternative in self.rank_dict[rank]:
+                self.weights[alternative] = self._calculate_weight(rank)
 
         sum_weights = sum(self.weights.values())
         for alternative in self.weights.keys():
@@ -43,14 +49,12 @@ class Srf:
     def _calculate_weight(self, rank):
         return 1 + (self.ratio - 1) * (rank - 1) / (self.r_v - 1)
 
-    def get_weights(self):
-        return self.weights
-
 
 if __name__ == "__main__":
     rank_dict = {1: 'g1', 2: 'white_card', 3: 'white_card', 4: ['g6', 'g8'], 5: 'g5', 6: 'white_card', 7: 'white_card',
                  8: 'white_card', 9: ['g2', 'g3', 'g7'], 10: 'white_card', 11: 'g4'}
     srf = Srf(rank_dict, 10)
+    srf.calculate_weights()
     # print the dictionary sorted by key
-    print({k: srf.weights[k] for k in sorted(srf.get_weights())})
+    print({k: srf.weights[k] for k in sorted(srf.weights)})
 
