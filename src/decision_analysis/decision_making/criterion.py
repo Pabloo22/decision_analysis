f3e640a76fd94e6e62ca2edd8d26c453201f55da
Pabloo22 <pablete.arino@gmail.com>
2023-03-28 20:41:45 +0200

    add plot method

--- src/decision_analysis/decision_making/criterion.py
+++ src/decision_analysis/decision_making/criterion.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import matplotlib.pyplot as plt
 from typing import Optional
 
 
@@ -20,3 +21,23 @@ class Criterion:
     preference_threshold: Optional[float] = None
     indifference_threshold: Optional[float] = None
     veto_threshold: Optional[float] = None
+
+    def plot_thresholds(self):
+        if self.preference_threshold is None:
+            self.preference_threshold = 0
+        if self.indifference_threshold is None:
+            self.indifference_threshold = 0
+        if self.veto_threshold is None:
+            self.veto_threshold = 0
+        x = [self.indifference_threshold, self.preference_threshold, self.veto_threshold]
+        y = [0, self.weight, 0]
+        plt.plot(x, y)
+        plt.title(self.name)
+        plt.xlabel("Value")
+        plt.ylabel("Weight")
+        plt.show()
+
+
+if __name__ == "__main__":
+    c = Criterion(1, name="Criterion 1", preference_threshold=0.5, indifference_threshold=0.1, veto_threshold=0.9)
+    c.plot_thresholds()
