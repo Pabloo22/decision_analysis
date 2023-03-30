00dab42a894673b56fa2a7aa012a036aaee21a11
Pabloo22 <pablete.arino@gmail.com>
2023-03-30 18:52:27 +0200

    Minor changes

--- examples/venice_trip/dominated_alternatives.py
+++ examples/venice_trip/dominated_alternatives.py
@@ -1,5 +1,6 @@
-from load_data import load_dataset, load_criteria
+import collections
 
+from load_data import load_dataset, load_criteria
 from src.decision_analysis.decision_making import find_dominated_alternatives
 
 
@@ -7,8 +8,15 @@ def print_dominated_alternatives():
     df = load_dataset()
     criteria = load_criteria()
     criteria_types = [c.type for c in criteria]
+    result = find_dominated_alternatives(df, criteria_types)
+    dominated_dominating_dict = collections.defaultdict(set)
+    for alternative, dominated in result.items():
+        for d in dominated:
+            dominated_dominating_dict[d].add(alternative)
+
     print('Dominated alternatives:')
-    print(find_dominated_alternatives(df, criteria_types))
+    for alternative, dominated in dominated_dominating_dict.items():
+        print(f'{alternative} is dominated by {dominated}')
 
 
 if __name__ == '__main__':
