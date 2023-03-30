f436bbfb9df646261d0526837e8c5c2ffa64aa74
Álvaro Laguna <72698468+alvarola17@users.noreply.github.com>
2023-03-30 23:43:27 +0200

    Fixed promethee_results.py

--- examples/venice_trip/promethee_results.py
+++ examples/venice_trip/promethee_results.py
@@ -5,11 +5,11 @@ from decision_analysis.decision_making import Promethee
 from load_data import load_dataset, load_criteria
 
 
-def draw_graph(graph):
+def draw_graph(graph, name):
     plt.figure(figsize=(10, 10))
     nx.draw(graph, with_labels=True, node_size=1000, node_color="lightblue", font_size=16, font_weight="bold",
             edgecolors="black", linewidths=2, alpha=0.9, width=2, font_color="black", arrowsize=20, arrowstyle="->")
-    plt.show()
+    plt.savefig(name)
 
 
 def promethee_results(version: int = 1):
@@ -31,10 +31,10 @@ def promethee_results(version: int = 1):
     print(promethee.get_dict_of_lists('II'))
 
     # Draw the graphs
-    draw_graph(g_i)
-    draw_graph(g_ii)
-    draw_graph(positive_flow)
-    draw_graph(negative_flow)
+    draw_graph(g_i, f'P_I_v{version}.png')
+    draw_graph(g_ii, f'P_II_v{version}.png')
+    draw_graph(positive_flow, f'P+_v{version}.png')
+    draw_graph(negative_flow, f'P-v_{version}.png')
 
 
 
