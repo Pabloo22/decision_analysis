c8496a8fd34dda40748f22813a132b9ff32e22b6
Pabloo22 <pablete.arino@gmail.com>
2023-04-09 17:49:43 +0200

    Add class Ranking and update class Criterion

--- src/decision_analysis/decision_making/criterion.py
+++ src/decision_analysis/decision_making/criterion.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import functools
 from typing import Optional
 
 
@@ -16,6 +17,9 @@ class Criterion:
         value_function_characteristic_points_location (list[float]): Location of the characteristic points of the value
             function in percentage with respect to the minimum and maximum values of the data for that criterion.
             Optional.
+        value_function_characteristic_points (list[tuple[float, float]]): Characteristic points of the value function
+            defined as a list of tuples (x, y) where `x` is the value of the criterion and y is the value of the value
+            function. Linear interpolation is used to calculate the value of the value function at intermediate values.
     """
     type: int
     weight: float = 1.
@@ -24,6 +28,7 @@ class Criterion:
     indifference_threshold: Optional[float] = None
     veto_threshold: Optional[float] = None
     value_function_characteristic_points_location: Optional[list[float]] = None
+    value_function_characteristic_points: Optional[list[tuple[float, float]]] = None
 
     _default_name = 'g1'
 
@@ -41,3 +46,24 @@ class Criterion:
     def reset_default_name(self) -> None:
         """Resets the default name of the criterion."""
         self._default_name = 'g1'
+
+    @staticmethod
+    def linear_interpolation(x: float, x1: float, x2: float, y1: float, y2: float) -> float:
+        """Returns the linear interpolation at the point x."""
+        return y1 + (y2 - y1) * (x - x1) / (x2 - x1)
+
+    @functools.cache
+    def value_function(self, value: float) -> float:
+        """Returns the value of the value function at the given value."""
+        if self.value_function_characteristic_points is None:
+            raise ValueError("The value function characteristic points must be defined")
+
+        sorted_characteristic_points = sorted(self.value_function_characteristic_points)
+        for i, (x, y) in enumerate(self.value_function_characteristic_points):
+            if value >= x:
+                continue
+            if i == 0:
+                return y
+
+            previous_x, previous_y = self.value_function_characteristic_points[i - 1]
+            return self.linear_interpolation(value, previous_x, x, previous_y, y)
---
+++ src/decision_analysis/decision_making/ranking.py
@@ -0,0 +1,135 @@
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from typing import Optional, Union
+
+
+class Ranking:
+    """A class representing a ranking of alternatives.
+
+    A ranking is a graph where each node is an alternative and each edge represents a preference relation between the
+    alternatives. The ranking is represented as a matrix where the rows and columns are the alternatives and the value
+    in the cell (i, j) is 1 if the alternative i is preferred to the alternative j, 0.5 if the alternatives are
+    indifferent and 0 if the alternative j is preferred to the alternative i.
+
+    Args:
+        alternatives (int or list[str]): The number of alternatives or a list with the names of the alternatives. If
+            the number of alternatives is given, the alternatives will be named as a_1, a_2, a_3, ...
+
+    Attributes:
+        alternative_names (list): List with the names of the alternatives.
+        matrix (np.array): Matrix representing the ranking. The rows and columns are the alternatives and the
+            value in the cell (i, j) is 1 if the alternative i is preferred to the alternative j, 0.5 if the
+            alternatives are indifferent and 0 if the alternative j is preferred to the alternative i
+    """
+
+    def __init__(self, alternatives: Union[int, list[str]], matrix: Optional[np.ndarray] = None):
+        if isinstance(alternatives, int):
+            alternatives = [f'a_{i}' for i in range(1, alternatives + 1)]
+        self.alternative_names = alternatives
+        self.matrix = np.zeros((self.n_alternatives, self.n_alternatives)) if matrix is None else matrix
+
+    @property
+    def n_alternatives(self) -> int:
+        return len(self.alternative_names)
+
+    def get_preference_relations(self) -> list[tuple[str, str]]:
+        """List with the preference relations in the ranking."""
+        preference_relations = []
+        for i in range(self.n_alternatives):
+            for j in range(self.n_alternatives):
+                if self.matrix[i, j] != 1:
+                    continue
+                preference_relations.append((self.alternative_names[i], self.alternative_names[j]))
+        return preference_relations
+
+    def get_indifference_relations(self) -> list[tuple[str, str]]:
+        """List with the indifference relations in the ranking."""
+        indifference_relations = []
+        for i in range(self.n_alternatives):
+            for j in range(self.n_alternatives):
+                if self.matrix[i, j] != 0.5:
+                    continue
+                indifference_relations.append((self.alternative_names[i], self.alternative_names[j]))
+        return indifference_relations
+
+    def visualize(self, title: Optional[str] = None, seed: Optional[int] = None):
+        """Visualize the ranking.
+
+        Args:
+            title (str): Title of the plot.
+            seed (int): Seed for the random number generator.
+        """
+
+    def _get_alternative_index(self, alternative: Union[int, str]) -> int:
+        """Get the index of an alternative.
+
+        Args:
+            alternative (int or str): The alternative.
+
+        Returns:
+            int: The index of the alternative.
+        """
+        if isinstance(alternative, str):
+            return self.alternative_names.index(alternative)
+        return alternative
+
+    def add_preference(self, alternative_i: Union[int, str], alternative_j: Union[int, str]):
+        """Add a preference relation between two alternatives.
+
+        Args:
+            alternative_i (int or str): The first alternative.
+            alternative_j (int or str): The second alternative.
+        """
+        alternative_i = self._get_alternative_index(alternative_i)
+        alternative_j = self._get_alternative_index(alternative_j)
+        self.matrix[alternative_i, alternative_j] = 1
+        self.matrix[alternative_j, alternative_i] = 0
+
+    def add_indifference(self, alternative_i: Union[int, str], alternative_j: Union[int, str]):
+        """Add an indifference relation between two alternatives.
+
+        Args:
+            alternative_i (int or str): The first alternative.
+            alternative_j (int or str): The second alternative.
+        """
+        alternative_i = self._get_alternative_index(alternative_i)
+        alternative_j = self._get_alternative_index(alternative_j)
+        self.matrix[alternative_i, alternative_j] = 0.5
+        self.matrix[alternative_j, alternative_i] = 0.5
+
+    def add_incomparability(self, alternative_i: Union[int, str], alternative_j: Union[int, str]):
+        """Add an incomparability relation between two alternatives.
+
+        Args:
+            alternative_i (int or str): The first alternative.
+            alternative_j (int or str): The second alternative.
+        """
+        alternative_i = self._get_alternative_index(alternative_i)
+        alternative_j = self._get_alternative_index(alternative_j)
+        self.matrix[alternative_i, alternative_j] = 0
+        self.matrix[alternative_j, alternative_i] = 0
+
+    def kendall_distance(self, other: 'Ranking') -> float:
+        """Calculate the Kendall distance between two rankings.
+
+        Args:
+            other (Ranking): The other ranking.
+
+        Returns:
+            float: The Kendall distance.
+        """
+        return np.sum(np.abs(self.matrix - other.matrix)) / 2
+    
+    def kendall_tau(self, other: 'Ranking') -> float:
+        """Calculate the Kendall tau between two rankings.
+
+        Args:
+            other (Ranking): The other ranking.
+
+        Returns:
+            float: The Kendall tau.
+        """
+        return 1 - 4 * self.kendall_distance(other) / (self.n_alternatives * (self.n_alternatives - 1))
