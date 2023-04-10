8a1ce1947b1f4d2cf981043e0ed5943cc94a87b1
Pabloo22 <pablete.arino@gmail.com>
2023-04-10 12:08:12 +0200

    Add test for comparisons

--- tests/test_ranking.py
+++ tests/test_ranking.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from src.decision_analysis.decision_making import Ranking
+from src.decision_analysis.decision_making import Ranking, Comparison, ComparisonType
 
 
 def ranking1():
@@ -33,6 +33,63 @@ def ranking3():
 ])
 def test_from_dict(ranking_dict, expected):
     assert np.allclose(Ranking.from_dict(ranking_dict).matrix, expected.matrix)
+
+
+def test_ranking_from_dict2():
+    ranking_dict = {'x': 1, 'y': 2, 'z': 2}
+    ranking = Ranking.from_dict(ranking_dict)
+    assert ranking.n_alternatives == 3
+    assert ranking.alternative_names == ['x', 'y', 'z']
+    expected_matrix = np.array([[0, 1, 1],
+                                [0, 0, 0.5],
+                                [0, 0.5, 0]])
+    assert np.array_equal(ranking.matrix, expected_matrix)
+
+
+def test_ranking_from_comparisons():
+    comparisons = [
+        Comparison(0, 1, ComparisonType.PREFERENCE),
+        Comparison(1, 2, ComparisonType.INDIFFERENCE),
+    ]
+    ranking = Ranking(alternatives=3)
+    ranking.from_comparisons(comparisons)
+    expected_matrix = np.array([[0, 1, 0], [0, 0, 0.5], [0, 0.5, 0]])
+    assert np.array_equal(ranking.matrix, expected_matrix)
+
+
+def test_ranking_get_comparisons():
+    matrix = np.array([[0, 1, 0],
+                       [0, 0, 0.5],
+                       [0, 0.5, 0]])
+    ranking = Ranking(matrix=matrix)
+    comparisons = ranking.get_comparisons()
+    expected_comparisons = [
+        Comparison(0, 1, ComparisonType.PREFERENCE),
+        Comparison(1, 2, ComparisonType.INDIFFERENCE),
+    ]
+    assert comparisons == expected_comparisons
+
+
+def test_ranking_remove_comparisons():
+    matrix = np.array([[0, 1, 0], [0, 0, 0.5], [0, 0.5, 0]])
+    ranking = Ranking(matrix=matrix)
+    ranking.remove_comparisons([
+        Comparison(0, 1, ComparisonType.PREFERENCE),
+        Comparison(1, 2, ComparisonType.INDIFFERENCE),
+    ])
+    assert np.array_equal(ranking.matrix, np.zeros((3, 3)))
+
+
+def test_init_without_arguments():
+    with pytest.raises(ValueError):
+        Ranking()
+
+
+def test_ranking_init_with_alternatives():
+    ranking = Ranking(alternatives=3)
+    assert ranking.n_alternatives == 3
+    assert ranking.alternative_names == ['a_1', 'a_2', 'a_3']
+    assert np.array_equal(ranking.matrix, np.zeros((3, 3)))
 
 
 @pytest.mark.parametrize("ranking_a, ranking_b, expected", [
