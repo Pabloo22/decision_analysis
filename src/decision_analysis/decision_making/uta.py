9d2f22f1a2dda5262a5ddc2c68296709c1410a36
Pabloo22 <pablete.arino@gmail.com>
2023-04-10 18:45:02 +0200

    Advances in UTA

--- src/decision_analysis/decision_making/uta.py
+++ src/decision_analysis/decision_making/uta.py
@@ -1,20 +1,52 @@
 import pulp
 
-from src.decision_analysis.decision_making import Dataset, Ranking, Comparison
+from src.decision_analysis.decision_making import Dataset, Ranking, Comparison, ComparisonType
 
 
 class UTA:
-    def __init__(self, dataset: Dataset, comparisons: Comparison):
+    """Class that implements the UTA method.
+
+    It uses linear programming in order to optimally infer additive value functions, so that these functions are
+    as consistent as possible with the global decision-maker’s preferences.
+
+    The optimal value functions are found by inferring the value of some specific breakpoints of the value
+    functions. These breakpoints are defined when creating the criteria of the dataset by setting
+    the characteristic_points_locations attribute of the value function of each criterion.
+
+    This class also implements the method to find the minimal inconsistent subset of comparisons.
+
+    Attributes:
+        dataset: The dataset that we want to analyze.
+        comparisons: The comparisons made by the decision maker.
+    """
+    def __init__(self, dataset: Dataset, comparisons: Comparison, epsilon: float = 0.0001):
         self.dataset = dataset
 
-        self.find_value_functions_model = pulp.LpProblem("Find value functions", pulp.LpMinimize)
-        self.find_inconsistency_model = pulp.LpProblem("Find inconsistency", pulp.LpMinimize)
+        self.value_functions_prob = pulp.LpProblem("Find value functions", pulp.LpMinimize)
+        self.inconsistency_prob = pulp.LpProblem("Find inconsistency", pulp.LpMinimize)
         self.comparisons = comparisons
         self.preference_info_ranking = Ranking(alternatives=dataset.alternative_names)
         self.preference_info_ranking.add_comparisons(comparisons)
 
+        self.epsilon = epsilon
+
     def find_minimal_inconsistent_subset(self, ranking: Ranking) -> list[Comparison]:
         """Finds a minimal subset of constraints that need to be removed to restore consistency.
+
+        The objective function consist on minimizing the sum of the binary variables that represent the
+        comparisons between alternatives. We call these variables v_i. The index i is the position of the
+        comparison in the list of self.comparisons. The value of the variable is 1 if the comparison
+        is inconsistent and 0 otherwise.
+
+        The specific constraints of this model can be represented as follows:
+        U(a_1) > U(a_2) - v_1 if a_1 is preferred to a_2 by the decision maker. This is represented by:
+        U(a_2) - U(a_1) - v_1 <= epsilon
+
+        U(a_1) is the comprehensive value of alternative a_1. It is the sum of the value functions of all
+        the criteria:
+
+        U(a_1) = sum_{i=1}^{n} u_i(g_i(a_1)) where u_i is the value function of criterion i and g_i is the
+        performance function of criterion (the value of the alternative in the criterion).
 
         Args:
             ranking: The ranking that we want to check for consistency. It does not need to be complete. If the
@@ -24,28 +56,131 @@ class UTA:
         Returns:
             A list of Comparison objects that need to be removed to restore consistency.
         """
+        inconsistency_vars = pulp.LpVariable.dicts("inconsistency", range(len(self.comparisons)), cat="Binary")
+
+        # Objective function
+        self.inconsistency_prob += pulp.lpSum(inconsistency_vars)
+
+        # Constraints for inconsistent comparisons
+        for idx, comparison in enumerate(self.comparisons):
+            alternative_i = comparison.alternative_1
+            alternative_j = comparison.alternative_2
+            U_ai = self._get_comprehensive_value_equation(alternative_i)
+            U_aj = self._get_comprehensive_value_equation(alternative_j)
+
+            # Only "<=" comparisons are allowed
+            if comparison.type == ComparisonType.PREFERENCE:
+                self.inconsistency_prob += U_aj - U_ai - inconsistency_vars[idx] <= self.epsilon
+            elif comparison.type == ComparisonType.INDIFFERENCE:
+                self.inconsistency_prob += U_ai - U_aj - inconsistency_vars[idx] <= self.epsilon
+                self.inconsistency_prob += U_aj - U_ai - inconsistency_vars[idx] <= self.epsilon
+
+        self.inconsistency_prob.solve(pulp.GLPK())
+
+        return self._get_inconsistent_comparisons()
 
     def _get_inconsistent_comparisons(self) -> list[Comparison]:
-        """Creates the inconsistent comparisons based on the results of the model.
+        """Creates the inconsistent comparisons based on the results of the inconsistency model.
+
+        If the binary variable that represents the comparison between two alternatives is 1, then the comparison
+        is inconsistent.
+
+        This is an auxiliary function for the method find_minimal_inconsistent_subset that is
+        called after solving the inconsistency model.
 
         Returns:
             A list of Comparison objects that need to be removed to restore consistency.
         """
+        inconsistent_comparisons = []
+        for idx, var in enumerate(self.inconsistency_prob.variables()):
+            if var.varValue == 1:
+                inconsistent_comparisons.append(self.comparisons[idx])
+        return inconsistent_comparisons
 
     def update_value_functions(self) -> None:
         """Updates the value functions of the dataset based on the results of the model.
 
         The value functions are the values of the objective function for each alternative.
         """
+        for criterion in self.dataset.criteria:
+            values = [self.value_functions_prob.variables()[i].varValue
+                      for i in range(len(self.dataset.alternative_names))]
+            criterion.value_function.characteristic_points_values = values
 
-    def _add_general_constraints(self, model: pulp.LpProblem) -> None:
+    def _get_comprehensive_value_equation(self, alternative_idx) -> pulp.LpVariable:
+        """Creates the comprehensive value equation.
+
+        The comprehensive value equation is the objective function of the model. It is the sum of the
+        value functions of all the criteria.
+
+        Args:
+            alternative_idx: The index of the alternative that we want to evaluate.
+        """
+
+    def _add_general_constraints(self, prob: pulp.LpProblem) -> None:
         """Adds the general constraints to the model.
 
         The general constraints are the ones that are common to both ordinal regression problems:
-            - Normalization constraint.
-            - Monotonicity constraint.
-            - Non-negativity constraint.
+            * Normalization constraint:
+                sum_{i=1}^{n} u_i(g_i(a)) = 1 for all a in A.
+
+            * Monotonicity constraint:
+                u_i(x_j) <= u_i(x_{j+1}) where x_j is the value_function.characteristic_points_locations[j]
+                of dataset.criteria[i].
+
+            * Non-negativity constraint: All variables must be non-negative.
+
+        This constraints will be used to find the value functions and the minimal inconsistent subset.
+
+        Note that this method must be called after adding the value function into the model.
 
         Args:
-            model: The model to which the constraints are added.
+            prob: The problem in which the constraints are added.
         """
+
+    def get_comprehensive_values(self) -> dict[str, float]:
+        """Gets the comprehensive values of the alternatives.
+
+        They are computed as the sum of the value functions of each criterion.
+
+        Returns:
+            A dictionary with the comprehensive values of the alternatives.
+        """
+        comprehensive_values = {}
+        for alternative in self.dataset.alternative_names:
+            U_a = sum(self.dataset.criteria[k].value_function(self.dataset.data[alternative, k]) for k in
+                      range(len(self.dataset.criteria)))
+            comprehensive_values[alternative] = U_a
+        return comprehensive_values
+
+    def create_ranking(self) -> Ranking:
+        """Creates a ranking based on the value functions of the alternatives.
+
+        Returns:
+            A ranking of the alternatives.
+        """
+        comprehensive_values = self.get_comprehensive_values()
+        sorted_comprehensive_values = sorted(comprehensive_values.items(), key=lambda x: x[1], reverse=True)
+        ranking_dict = {}
+        rank = 1
+        last_value = -1
+        for alternative, value in sorted_comprehensive_values:
+            ranking_dict[alternative] = rank
+            if value == last_value:
+                continue
+            rank += 1
+            last_value = value
+
+        return Ranking.from_dict(ranking_dict)
+
+    @staticmethod
+    def print_model_results(prob) -> None:
+        """Prints the results of the value functions model.
+
+        The results are the value functions of the criteria.
+        """
+        print(f"Status: {pulp.LpStatus[prob.status]}")
+        print("Variables:")
+        for var in prob.variables():
+            print(f"{var.name} = {var.varValue}")
+        print(f"Objective function value: {pulp.value(prob.objective)}")
