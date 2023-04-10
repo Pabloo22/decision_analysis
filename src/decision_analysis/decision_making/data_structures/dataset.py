c16591dad5db0aec32027e7ef36950e1fbf0c52a
Pabloo22 <pablete.arino@gmail.com>
2023-04-10 14:45:10 +0200

    Reformat class Dataset

--- src/decision_analysis/decision_making/data_structures/dataset.py
+++ src/decision_analysis/decision_making/data_structures/dataset.py
@@ -1,55 +1,36 @@
+from typing import Optional
 import numpy as np
-
-from typing import Optional, Union
+from dataclasses import dataclass
 
 from .criterion import Criterion
 
 
+@dataclass
 class Dataset:
     """A dataset is a collection of data and criteria.
 
     We have decided to not make use of the Alternative class here, alternatives will be encoded as integers.
     However, it is possible to associate a name to each one.
+
+    Attributes:
+        data: A numpy array containing the data.
+        criteria: A list of criteria.
+        alternative_names: A list of alternative names. If not given, the alternatives will be named
+            as a_1, a_2, a_3, ...
     """
+    data: np.ndarray
+    criteria: list[Criterion]
+    alternative_names: Optional[list[str]] = None
 
-    def __init__(self,
-                 data: np.ndarray,
-                 criteria: list[Criterion],
-                 alternative_names: Optional[list[str]] = None):
-
-        if not isinstance(data, np.ndarray):
+    def __post_init__(self):
+        if not isinstance(self.data, np.ndarray):
             raise TypeError("Data must be a numpy array")
 
-        if data.shape[1] != len(criteria):
+        if self.data.shape[1] != len(self.criteria):
             raise ValueError("The number of columns in the data must be equal to the number of criteria")
 
-        if alternative_names is not None:
-            if len(alternative_names) != data.shape[0]:
+        if self.alternative_names is not None:
+            if len(self.alternative_names) != self.data.shape[0]:
                 raise ValueError("The number of alternative names must be equal to the number of alternatives")
-
-            self.alternative_names = alternative_names
         else:
-            self.alternative_names = [f'a_{i+1}' for i in range(data.shape[0])]
-
-        self.data = data
-        self.criteria = criteria
-        self.preference_relations = []
-        self.indifference_relations = []
-
-    def add_preference(self, alternative_1: Union[int, str], alternative_2: Union[int, str]):
-        if isinstance(alternative_1, str):
-            alternative_1 = self.alternative_names.index(alternative_1)
-        if isinstance(alternative_2, str):
-            alternative_2 = self.alternative_names.index(alternative_2)
-        self.preference_relations.append((alternative_1, alternative_2))
-
-    def add_indifference(self, alternative_1: Union[int, str], alternative_2: Union[int, str]):
-        if isinstance(alternative_1, str):
-            alternative_1 = self.alternative_names.index(alternative_1)
-        if isinstance(alternative_2, str):
-            alternative_2 = self.alternative_names.index(alternative_2)
-        self.indifference_relations.append((alternative_1, alternative_2))
-
-
-
-
+            self.alternative_names = [f'a_{i + 1}' for i in range(self.data.shape[0])]
