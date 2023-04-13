bbaac55286f18448767def30a988f77eb714b745
Álvaro Laguna <72698468+alvarola17@users.noreply.github.com>
2023-04-13 20:06:35 +0200

    Added comparisons

--- examples/venice_trip/comparisons.py
+++ examples/venice_trip/comparisons.py
@@ -1,5 +1,5 @@
 """In this script, we will find some pairwise comparisons based on the idea that if the difference
-in price is not too big, then an accomadation near Venice is preferable to one further away."""
+in price is not too big, then an accommodation near Venice is preferable to one further away."""
 
 from decision_analysis.decision_making import Comparison, ComparisonType
 
@@ -7,7 +7,6 @@ from load_data import load_dataset
 
 
 def find_comparisons(price_difference: float = 25):
-
     dataset = load_dataset()
 
     for i, row in dataset.iterrows():
@@ -44,6 +43,42 @@ def get_comparisons():
         Comparison(4, 6, ComparisonType.PREFERENCE),  # Least favorable option
         Comparison(5, 4, ComparisonType.PREFERENCE)  # Check inconsistency detection
     ]
+    return comparisons
+
+
+def initial_pairwise_comparisons() -> list:
+    comparisons = [Comparison(4, 0, ComparisonType.PREFERENCE),  # Initial pairwise comparison
+                   Comparison(4, 1, ComparisonType.PREFERENCE),  # Initial pairwise comparison
+                   Comparison(4, 2, ComparisonType.PREFERENCE),  # Initial pairwise comparison
+                   Comparison(4, 3, ComparisonType.PREFERENCE)]  # Initial pairwise comparison]
+
+    return comparisons
+
+
+def proximity_to_venice_and_low_price_difference() -> list:
+    comparisons = [Comparison(0, 3, ComparisonType.PREFERENCE),  # Proximity to Venice and low price difference
+                   Comparison(1, 3, ComparisonType.PREFERENCE),  # Proximity to Venice and low price difference
+                   Comparison(4, 7, ComparisonType.PREFERENCE),  # Proximity to Venice and low price difference
+                   Comparison(10, 8, ComparisonType.PREFERENCE)]  # Proximity to Venice and low price difference
+
+    return comparisons
+
+
+def commodity_low_price_difference_same_location() -> list:
+    comparisons = [Comparison(0, 1, ComparisonType.PREFERENCE)]  # Commodity, low price difference, same location
+
+    return comparisons
+
+
+def least_favorable_option() -> list:
+    comparisons = [Comparison(i, 6, ComparisonType.PREFERENCE) for i in range(12) if i != 6]  # Least favorable option
+
+    return comparisons
+
+
+def check_inconsistency_detection() -> list:
+    comparisons = [Comparison(5, 4, ComparisonType.PREFERENCE)]  # Check inconsistency detection
+
     return comparisons
 
 
