a5416fb5955ce348d6c58f22c30f70fb81324cd4
Álvaro Laguna <72698468+alvarola17@users.noreply.github.com>
2023-04-13 13:20:29 +0200

    Fixed value function plotting

--- examples/venice_trip/uta_ranking.py
+++ examples/venice_trip/uta_ranking.py
@@ -3,7 +3,6 @@ import pulp
 
 
 from comparisons import get_comparisons
-from decision_analysis.decision_making.data_structures.dataset import Dataset
 from decision_analysis.decision_making.uta import UTA
 from load_data import get_dataset
 
@@ -17,11 +16,12 @@ def plot_value_functions(prob: pulp.LpProblem):
     y = [[variables[v] for v in variables.keys() if v.startswith(f'u_{i}')] for i in range(1, number_of_plots + 1)]
     max_y = max([max(y[i]) for i in range(number_of_plots)])
     for u in range(1, number_of_plots + 1):
-        plt.plot(x[u - 1], y[u - 1], label=f'u_{u}')
-        plt.title(f'u_{u}')
-        plt.xlabel('x')
-        plt.ylabel('u(x)')
+        plt.plot(x[u - 1], y[u - 1])
+        plt.title(f'$u_{u}$')
+        plt.xlabel('$x$')
+        plt.ylabel('$u(x)$')
         plt.ylim(0, max_y)
+        plt.xlim(x[u - 1][0], x[u - 1][-1])
         plt.show()
 
 
@@ -36,7 +36,7 @@ if __name__ == '__main__':
     uta.solve()
 
     UTA.print_problem_results(uta.prob)
-    # plot_value_functions(uta.prob)
+    plot_value_functions(uta.prob)
     uta.update_value_functions()
     uta_ranking = uta.create_ranking()
     uta_ranking.visualize("UTA Ranking", seed=42, layout='spring')
